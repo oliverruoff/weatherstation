5fdfe0446b738483b89707ccd2f944c02ad664b3
Oliver Ruoff <oliver93ruoff@web.de>
2024-06-02 19:27:37 +0200

    improved json handling

--- code/server.py
+++ code/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import time
 from sensors import ADS1115, BME280, RainDropSensor
+import json
 
 # Initialisierung der Sensoren
 ads1115 = ADS1115.ADS1115(i2c_address=0x48)
@@ -13,36 +14,42 @@ app = Flask(__name__)
 def format_value(value):
     return round(value, 2)
 
+def get_safe_json(data):
+    try:
+        return json.dumps(data)
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
 @app.route('/battery', methods=['GET'])
 def get_battery_voltage():
     voltage = format_value(ads1115.get_channel_0_voltage())
-    return jsonify({'Battery Voltage': voltage})
+    return get_safe_json({'Battery Voltage': voltage})
 
 @app.route('/solar', methods=['GET'])
 def get_solar_voltage():
     voltage = format_value(ads1115.get_channel_1_voltage())
-    return jsonify({'Solar Voltage': voltage})
+    return get_safe_json({'Solar Voltage': voltage})
 
 @app.route('/wind', methods=['GET'])
 def get_wind_voltage():
     voltage = format_value(ads1115.get_channel_2_voltage())
-    return jsonify({'Wind Voltage': voltage})
+    return get_safe_json({'Wind Voltage': voltage})
 
 @app.route('/bme280', methods=['GET'])
 def get_bme280_data():
     bme_data = bme280.get_all()
-    return jsonify({
-        'Temperature': format_value(bme_data['temperature']),
-        'Pressure': format_value(bme_data['pressure']),
-        'Humidity': format_value(bme_data['humidity'])
+    return get_safe_json({
+        'Temperature': format_value(bme_data.get('temperature', 0)),
+        'Pressure': format_value(bme_data.get('pressure', 0)),
+        'Humidity': format_value(bme_data.get('humidity', 0))
     })
 
 @app.route('/rain', methods=['GET'])
 def get_rain_status():
     if rain_sensor.is_raining():
-        return jsonify({'Rain Status': 'It is raining.'})
+        return get_safe_json({'Rain Status': 'It is raining.'})
     else:
-        return jsonify({'Rain Status': 'It is dry.'})
+        return get_safe_json({'Rain Status': 'It is dry.'})
 
 @app.route('/all', methods=['GET'])
 def get_all_data():
@@ -52,13 +59,13 @@ def get_all_data():
     bme_data = bme280.get_all()
     rain_status = 'It is raining.' if rain_sensor.is_raining() else 'It is dry.'
 
-    return jsonify({
+    return get_safe_json({
         'Battery Voltage': battery_voltage,
         'Solar Voltage': solar_voltage,
         'Wind Voltage': wind_voltage,
-        'Temperature': format_value(bme_data['temperature']),
-        'Pressure': format_value(bme_data['pressure']),
-        'Humidity': format_value(bme_data['humidity']),
+        'Temperature': format_value(bme_data.get('temperature', 0)),
+        'Pressure': format_value(bme_data.get('pressure', 0)),
+        'Humidity': format_value(bme_data.get('humidity', 0)),
         'Rain Status': rain_status
     })
 
