7db475089961e339885fc4b64bcc81e5a93b2c51
Oliver Ruoff <oliver93ruoff@web.de>
2024-06-02 18:00:18 +0200

    reduced to two floating values

--- code/server.py
+++ code/server.py
@@ -10,28 +10,31 @@ rain_sensor = RainDropSensor.RainDropSensor(pin=18)
 # Flask-App initialisieren
 app = Flask(__name__)
 
+def format_value(value):
+    return round(value, 2)
+
 @app.route('/battery', methods=['GET'])
 def get_battery_voltage():
-    voltage = ads1115.get_channel_0_voltage()
+    voltage = format_value(ads1115.get_channel_0_voltage())
     return jsonify({'Battery Voltage': voltage})
 
 @app.route('/solar', methods=['GET'])
 def get_solar_voltage():
-    voltage = ads1115.get_channel_1_voltage()
+    voltage = format_value(ads1115.get_channel_1_voltage())
     return jsonify({'Solar Voltage': voltage})
 
 @app.route('/wind', methods=['GET'])
 def get_wind_voltage():
-    voltage = ads1115.get_channel_2_voltage()
+    voltage = format_value(ads1115.get_channel_2_voltage())
     return jsonify({'Wind Voltage': voltage})
 
 @app.route('/bme280', methods=['GET'])
 def get_bme280_data():
     bme_data = bme280.get_all()
     return jsonify({
-        'Temperature': bme_data['temperature'],
-        'Pressure': bme_data['pressure'],
-        'Humidity': bme_data['humidity']
+        'Temperature': format_value(bme_data['temperature']),
+        'Pressure': format_value(bme_data['pressure']),
+        'Humidity': format_value(bme_data['humidity'])
     })
 
 @app.route('/rain', methods=['GET'])
@@ -43,9 +46,9 @@ def get_rain_status():
 
 @app.route('/all', methods=['GET'])
 def get_all_data():
-    battery_voltage = ads1115.get_channel_0_voltage()
-    solar_voltage = ads1115.get_channel_1_voltage()
-    wind_voltage = ads1115.get_channel_2_voltage()
+    battery_voltage = format_value(ads1115.get_channel_0_voltage())
+    solar_voltage = format_value(ads1115.get_channel_1_voltage())
+    wind_voltage = format_value(ads1115.get_channel_2_voltage())
     bme_data = bme280.get_all()
     rain_status = 'It is raining.' if rain_sensor.is_raining() else 'It is dry.'
 
@@ -53,9 +56,9 @@ def get_all_data():
         'Battery Voltage': battery_voltage,
         'Solar Voltage': solar_voltage,
         'Wind Voltage': wind_voltage,
-        'Temperature': bme_data['temperature'],
-        'Pressure': bme_data['pressure'],
-        'Humidity': bme_data['humidity'],
+        'Temperature': format_value(bme_data['temperature']),
+        'Pressure': format_value(bme_data['pressure']),
+        'Humidity': format_value(bme_data['humidity']),
         'Rain Status': rain_status
     })
 
